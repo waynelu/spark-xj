79babd96f377372c966d755481b08fee6f21e167
Lu <wlu000@CACSVML-14677.local>
2016-11-02 17:21:13 -0700

    tweak PI

--- src/main/scala/com/r2/xj/DataForecast.scala
+++ src/main/scala/com/r2/xj/DataForecast.scala
@@ -25,7 +25,7 @@ object DataForecast {
    * Creates a Spark DataFrame of (timestamp, symbol, price) from a tab-separated file of stock
    * ticker data.
    */
-  def loadObservations(sqlContext: SQLContext, path: String, tag: String): DataFrame = {
+  def loadObservationsFromCSV(sqlContext: SQLContext, path: String, tag: String): DataFrame = {
     val rowRdd = sqlContext.sparkContext.textFile(path).map { line =>
       val tokens = line.split(',')
       val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.systemDefault());
@@ -60,11 +60,13 @@ object DataForecast {
   }
 
   def loadObservationsFromPI(sqlContext: SQLContext, path: String, tag: String): DataFrame = {
-    val url = "jdbc:pioledb://r2win.ddns.net/Data Source=r2win.ddns.net; Integrated Security=SSPI"
+    //val url = "jdbc:pioledb://r2win.ddns.net/Data Source=r2win.ddns.net; Integrated Security=SSPI"
+    val url = "jdbc:pioledb://r2win.ddns.net/Data Source=r2win.ddns.net"
     val driverClassName = "com.osisoft.jdbc.Driver";
     Class.forName(driverClassName).newInstance();
     val connection = DriverManager.getConnection(url)
-    val pStatement = connection.prepareStatement("SELECT time, tag, value FROM picomp2 WHERE time >='2016-10-01' and time<='2016-10-12' and tag like ?");
+    val connection = DriverManager.getConnection(url, "Administrator", "")
+    val pStatement = connection.prepareStatement("SELECT time, tag, value FROM piinterp2 WHERE timestep='5m' and time >='2016-10-06 00:00:00' and time<='2016-10-12 00:00:00' and tag like ?");
     pStatement.setString(1, tag);
     val resultSet = pStatement.executeQuery();
 
@@ -92,7 +94,7 @@ object DataForecast {
     )
     val schema = StructType(fields)
     sqlContext.createDataFrame(rowRdd, schema)
-  }
+}
 
   def main(args: Array[String]): Unit = {
     val tag = args(0)
@@ -105,9 +107,9 @@ object DataForecast {
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    //val dataObs = loadObservations(sqlContext, "./data_201601/adata_301.csv", tag)
+    //val dataObs = loadObservationsFromCSV(sqlContext, "./data_201601/adata_301.csv", tag)
     //val dataObs = loadObservationsFromES(sqlContext, "xj-data/data", tag)
-val dataObs = loadObservationsFromPI(sqlContext, "", tag)
+    val dataObs = loadObservationsFromPI(sqlContext, "", tag)
     //println(dataObs.count)
     //return
     
@@ -115,15 +117,17 @@ val dataObs = loadObservationsFromPI(sqlContext, "", tag)
     // Create an daily DateTimeIndex over August and September 2015
     val zone = ZoneId.systemDefault()
     val dtIndex = DateTimeIndex.uniformFromInterval(
-      ZonedDateTime.of(LocalDateTime.parse("2016-01-01T00:00:00"), zone),
-      ZonedDateTime.of(LocalDateTime.parse("2016-01-11T00:00:00"), zone),
+      //ZonedDateTime.of(LocalDateTime.parse("2016-01-01T00:00:00"), zone), //CSV and ES
+      //ZonedDateTime.of(LocalDateTime.parse("2016-01-11T00:00:00"), zone), //CSV and ES
+      ZonedDateTime.of(LocalDateTime.parse("2016-10-07T00:00:00"), zone), //PI
+      ZonedDateTime.of(LocalDateTime.parse("2016-10-11T00:00:00"), zone), //PI
       //new MinuteFrequency(5))
       new HourFrequency(1))
       
     // Align the data on the DateTimeIndex to create a TimeSeriesRDD
     val dataTsrdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, dataObs,
-      "timestamp", "tag", "value")
-      //"tagtime", "tagid", "value")
+      "timestamp", "tag", "value") // CSV and PI
+      //"tagtime", "tagid", "value") // ES
 
     // Cache it in memory
     dataTsrdd.cache()
@@ -146,8 +150,10 @@ val dataObs = loadObservationsFromPI(sqlContext, "", tag)
     */
     
     //slice base ts rdd to prepare forecasting
-    val startDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-01-02T00:00:00"), zone)
-    val endDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-01-07T00:00:00"), zone)
+    //val startDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-01-02T00:00:00"), zone) // CSV and ES
+    //val endDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-01-07T00:00:00"), zone) // CSV and ES
+    val startDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-10-07T00:00:00"), zone) // PI
+    val endDateTime = ZonedDateTime.of(LocalDateTime.parse("2016-10-08T00:00:00"), zone) // PI
     val sampleTsrdd = filledTsrdd.slice(startDateTime, endDateTime) 
     
     val sampleTs = sampleTsrdd.findSeries(tag)
